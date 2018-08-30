5dc740b8598020d25b50f4887dc1d07106e7c3fd
Nick Ford <nickforddesign@gmail.com>
2018-08-30 12:24:52 -0400

    clean up webpack config, remove build folder

--- utils/build.js
+++ utils/build.js
@@ -1,6 +1,8 @@
 const webpack = require('webpack')
 const config = require('../webpack.config')
 
+delete config.notHotReload
+
 webpack(config, (err) => {
   if (err) {
     throw err
--- utils/webserver.js
+++ utils/webserver.js
@@ -5,8 +5,7 @@ const env = require('./env')
 const path = require('path')
 const chalk = require('chalk')
 
-const options = ({})
-const excludeEntriesToHotReload = (options.notHotReload || [])
+const excludeEntriesToHotReload = config.notHotReload || []
 
 for (let entryName in config.entry) {
   if (!excludeEntriesToHotReload.includes(entryName)) {
@@ -17,12 +16,16 @@ for (let entryName in config.entry) {
   }
 }
 
+delete config.notHotReload
+
 config.plugins = [new webpack.HotModuleReplacementPlugin()].concat(config.plugins || [])
 
 const compiler = webpack(config)
+
 compiler.plugin('done', () => {
   console.log(chalk.blueBright(`Webpack dev server listening on port ${env.PORT}`))
 })
+
 const server = new WebpackDevServer(compiler, {
   hot: true,
   quiet: true,

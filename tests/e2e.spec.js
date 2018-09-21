05422d08a968e7a7f1f1b889427cf354548c9a33
Nick Ford <nickforddesign@gmail.com>
2018-09-21 13:54:46 -0400

    set up some basic e2e tests

--- tests/e2e.spec.js
+++ tests/e2e.spec.js
@@ -1,12 +1,52 @@
 const { TESTING_PORT } = require('../utils/env')
+// const utils = require('../src/utils')
+
+const base_url = `http://localhost:${TESTING_PORT}`
+
+jest.setTimeout(40000)
 
 describe('e2e tests', async () => {
+
   beforeAll(async () => {
-    // jest.setTimeout(35000)
-    await page.goto(`http://localhost:${TESTING_PORT}`)
+    await page.goto(base_url)
   })
 
-  it('should display "google" text on page', async () => {
-    await expect(page).toMatch('google')
+  describe('unnamed inputs', async () => {
+
+    test('form rendered', async () => {
+      await page.goto(`${base_url}/unnamed_inputs.html`)
+      await expect(page).toMatch('Last Name')
+    })
+
+    test('unnamed inputs have a name', async () => {
+      const renamed_inputs = [
+        'phone1',
+        'phone2',
+        'phone3'
+      ]
+      const selector = renamed_inputs.map(name => {
+        return `input[name="${name}"]`
+      }).join(',')
+      const inputs = await page.$$eval(selector, divs => {
+        return divs.length
+      })
+      expect(inputs).toBe(renamed_inputs.length)
+    })
+
+    test('hidden inputs were added', async () => {
+      const hidden_inputs = [
+        'meta.form-id',
+        'meta.trackerid',
+        'CA-uid',
+        'CA-sess'
+      ]
+      const selector = hidden_inputs.map(name => {
+        return `input[name="${name}"]`
+      }).join(',')
+      const inputs = await page.$$eval(selector, divs => {
+        return divs.length
+      })
+      expect(inputs).toBe(hidden_inputs.length)
+    })
   })
 })

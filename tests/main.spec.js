1c781d440a73f5f0da7f7fab4e9e5c94c990b5db
Nick Ford <nickforddesign@gmail.com>
2018-09-26 12:23:55 -0400

    tests working

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -6,16 +6,17 @@ import fs from 'fs'
 import path from 'path'
 import { JSDOM } from 'jsdom'
 import * as utils from '../src/utils'
-// import template from '../dist/async.html'
-
-const template = fs.readFileSync(path.resolve(__dirname, `../dist/async.html`)).toString()
-
-const dom = new JSDOM(template)
-// console.log(dom.window.document)
+const template_path = path.resolve(__dirname, `../dist/async.html`)
+const template = fs.readFileSync(template_path).toString()
+const jsdom_options = {
+  resources: 'usable',
+  runScripts: 'dangerously'
+}
+const dom = new JSDOM(template, jsdom_options)
 
 const doc = dom.window.document
 
-// console.log(doc.body.outerHTML)
+console.log(doc.head.outerHTML)
 describe('async form', () => {
   test('should poll for async form', async () => {
     await utils.sleep(2000)
